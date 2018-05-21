a5d4ef3ef220e96797a82e4fc4b7fdaeadbf0bc1
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-05-21 09:56:29 +0300

    Multiple Highlight: Separated HTML, CSS and JavaScript parts.

---
+++ multiple_highlight2.js
@@ -0,0 +1,116 @@
+var helperObject;
+(function(helperObject) {
+
+    var mouseDown = false;
+    var selectionChanged = false;
+    var selectionProcessing = false;
+
+    var inputCtrl = document.getElementById('input');
+    var resultCtrl = document.getElementById('result');
+    var widthCtrl = document.getElementById('width');
+
+
+    helperObject.userInput = function() {
+        var input = inputCtrl.value;
+        input = replaceAngleBrackets(input);
+
+        resultCtrl.innerHTML = input;
+    }
+
+    helperObject.parameters = function() {
+        document.querySelector('#container-txt').style.width = widthCtrl.value;
+    }
+
+    helperObject.clearInput = function() {
+        inputCtrl.value = "";
+        resultCtrl.innerHTML = "";
+
+        inputCtrl.focus();
+    }
+
+    function replaceAngleBrackets(input) {
+        input = input.replace(/</g, '&lt');
+        input = input.replace(/>/g, '&gt');
+
+        return input;
+    }
+
+    document.onselectionchange = function(e) {
+        selectionChanged = true;
+    };
+
+    helperObject.mouseDown = function() {
+        mouseDown = true;
+    }
+
+    helperObject.mouseUp = function() {
+        mouseDown = false;
+    }
+    
+    setInterval(function() {
+        if (!selectionChanged || mouseDown) {
+            return;
+        }
+
+        if (selectionProcessing) {
+            return;
+        }
+
+        selectionProcessing = true;
+
+        processSelection();
+
+        selectionProcessing = false;
+        selectionChanged = false;
+    }, 100);
+
+    function processSelection() {
+        var selection = window.getSelection();
+
+        var txt = "";
+
+        var start = selection.anchorOffset;
+        var end = selection.focusOffset;
+        try {
+            txt = selection.anchorNode.textContent.substring(start, end);
+        }
+        catch {
+            return;
+        }
+
+        if (txt == '') {
+            return;
+        }
+        
+        txt = replaceAngleBrackets(txt);
+
+        var replaceRe;
+        try {
+            replaceRe = new RegExp('(' + txt + ')', 'gi');
+        }
+        catch {
+            return;
+        }
+
+        var resultTxt = replaceAngleBrackets(inputCtrl.value).replace(replaceRe, '<span class="selection-processed">$1</span>');
+
+        resultCtrl.innerHTML = resultTxt;
+    }
+
+    document.body.addEventListener('mousedown', helperObject.mouseDown);
+    document.body.addEventListener('mouseup', helperObject.mouseUp);
+    
+    helperObject.toggleInput = function() {
+        var styleCtrl = document.getElementById('userInput').style;
+        var toggleBtn = document.getElementById('toggleInput');
+        
+        if (styleCtrl.display == 'none') {
+            styleCtrl.display = '';
+            toggleBtn.innerText = 'Hide input';
+        }
+        else if (styleCtrl.display == ''){
+            styleCtrl.display = 'none';
+            toggleBtn.innerText = 'Show input';
+        }
+    }
+})(helperObject || (helperObject = {}));

8e5b355e48b211b5a2097bdbb088a11546cf93e1
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-06-26 14:38:10 +0300

    Changed tabs logic: event -> this (for simple open certaint tab when page opens).

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,4 @@
-function openTab(evt, tabName) {
+function openTab(element, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -16,5 +16,5 @@ function openTab(evt, tabName) {
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    element.classList.add("active");
 }
